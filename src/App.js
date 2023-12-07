c5e5ce80e6b60dd8fdd00db950b0bca12263abc4
Firefingers1167 <karthikeyanr482@gmail.com>
2023-12-07 13:28:59 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Page1 from './pages/page1';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Page1 mess={"Landing Page"}/>
+      <Page1 mess = {"Evaluation Page"}></Page1>
+      <Page1 mess = {"Result/Chatbot Page"}></Page1>
     </div>
   );
 }
---
+++ src/pages/page1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Page1(props) {
+    return (
+        <div className="one-page-container">
+          <h1>{props.mess}</h1>
+          {/* Add your content here */}
+        </div>
+      );
+}
+
+export default Page1
