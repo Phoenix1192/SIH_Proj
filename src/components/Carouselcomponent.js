2f2d638942496d6805ccb77537939d4e410e36cd
Nitro <cnitinrohit@gmail.com>
2023-12-11 17:28:24 +0530

    changed the stream section little

--- src/components/Carouselcomponent.js
+++ src/components/Carouselcomponent.js
@@ -6,14 +6,14 @@ const CarouselComponent = (props) => {
   return (
     <Carousel>
       <Carousel.Item>
-        <div className="boxu" style={{ height: 'auto', width: '80vw', margin: 'auto' }}>
+        <div className="boxu" style={{ height: 'auto', width: '100vw', margin: 'auto' }}>
           <h3 style={{textAlign:"left" , marginLeft:"2%"}}>Heading1</h3>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt quibusdam atque quas illo incidunt sapiente harum, eos expedita ab perferendis cum odit modi deleniti, fuga error exercitationem inventore ipsa. Animi sapiente ullam incidunt quo deserunt accusamus quam totam, maxime, sit placeat ipsa maiores. Reiciendis repellat ad numquam quia. Maiores, dolorum!
         </div>
       </Carousel.Item>
 
       <Carousel.Item>
-        <div  className= "boxu" style={{ height: 'auto', width: '80vw', margin: 'auto' }}>
+        <div  className= "boxu" style={{ height: 'auto', width: '100vw', margin: 'auto' }}>
         <h3 style={{textAlign:"left" , marginLeft:"2%"}}>Heading2</h3>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas officiis voluptatum cum hic dignissimos quisquam eos, reiciendis neque assumenda deleniti obcaecati! Accusamus sit sunt libero veniam. Provident beatae natus, fugit autem ut temporibus totam modi consectetur harum non architecto aspernatur dolorum dolore, dignissimos voluptas expedita vero impedit maiores. Temporibus, assumenda.
           
