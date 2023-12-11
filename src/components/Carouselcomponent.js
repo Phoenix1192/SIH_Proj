8f36aeac3378658049df51f715d136513beaddb0
Nitro <cnitinrohit@gmail.com>
2023-12-11 23:59:43 +0530

    added colors to the streams

--- src/components/Carouselcomponent.js
+++ src/components/Carouselcomponent.js
@@ -6,17 +6,23 @@ const CarouselComponent = (props) => {
   return (
     <Carousel>
       <Carousel.Item>
-        <div className="boxu" style={{ height: 'auto', width: '100vw', margin: 'auto' }}>
-          <h3 style={{textAlign:"left" , marginLeft:"2%"}}>Heading1</h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt quibusdam atque quas illo incidunt sapiente harum, eos expedita ab perferendis cum odit modi deleniti, fuga error exercitationem inventore ipsa. Animi sapiente ullam incidunt quo deserunt accusamus quam totam, maxime, sit placeat ipsa maiores. Reiciendis repellat ad numquam quia. Maiores, dolorum!
+        <div className="boxu sci" style={{ width: '100vw', margin: 'auto' }}>
+          <h3 style={{textAlign:"left" , marginLeft:"2%"}}>Science</h3>
+          <div className="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt quibusdam atque quas illo incidunt sapiente harum, eos expedita ab perferendis cum odit modi deleniti, fuga error exercitationem inventore ipsa. Animi sapiente ullam incidunt quo deserunt accusamus quam totam, maxime, sit placeat ipsa maiores. Reiciendis repellat ad numquam quia. Maiores, dolorum! hello world hello world</div>
         </div>
       </Carousel.Item>
 
       <Carousel.Item>
-        <div  className= "boxu" style={{ height: 'auto', width: '100vw', margin: 'auto' }}>
-        <h3 style={{textAlign:"left" , marginLeft:"2%"}}>Heading2</h3>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas officiis voluptatum cum hic dignissimos quisquam eos, reiciendis neque assumenda deleniti obcaecati! Accusamus sit sunt libero veniam. Provident beatae natus, fugit autem ut temporibus totam modi consectetur harum non architecto aspernatur dolorum dolore, dignissimos voluptas expedita vero impedit maiores. Temporibus, assumenda.
-          
+        <div  className= "boxu com" style={{ width: '100vw', margin: 'auto' }}>
+        <h3 style={{textAlign:"left" , marginLeft:"2%"}}>Commerce</h3>
+          <div className="content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas officiis voluptatum cum hic dignissimos quisquam eos, reiciendis neque assumenda deleniti obcaecati! Accusamus sit sunt libero veniam. Provident beatae natus, fugit autem ut temporibus totam modi consectetur harum non architecto aspernatur dolorum dolore, dignissimos voluptas expedita vero impedit maiores. Temporibus, assumenda.</div>
+        </div>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <div  className= "boxu art" style={{ width: '100vw', margin: 'auto' }}>
+        <h3 style={{textAlign:"left" , marginLeft:"2%"}}>Arts</h3>
+          <div className="content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas officiis voluptatum cum hic dignissimos quisquam eos, reiciendis neque assumenda deleniti obcaecati! Accusamus sit sunt libero veniam. Provident beatae natus, fugit autem ut temporibus totam modi consectetur harum non architecto aspernatur dolorum dolore, dignissimos voluptas expedita vero impedit maiores. Temporibus, assumenda.</div>
         </div>
       </Carousel.Item>
 
--- src/pages/page1.jsx
+++ src/pages/page1.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HomePage from '../components/homepage';
+import HomePage from '../components/homepage'
 
 function Page1(props) {
     return (
